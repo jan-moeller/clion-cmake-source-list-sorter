dafd5b7bf27ac84b952322f455212d0af4cf752f
Jan Möller <drag-on@gmx.de>
2024-07-10 22:52:49 +0200

    Implement service to manage plugin configuration

---
+++ src/main/kotlin/com/github/janmoeller/clioncmakesourcelistsorter/config/ConfigManagerService.kt
@@ -0,0 +1,17 @@
+package com.github.janmoeller.clioncmakesourcelistsorter.config
+
+import com.intellij.openapi.components.BaseState
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.SimplePersistentStateComponent
+import com.intellij.openapi.components.State
+import com.intellij.openapi.project.Project
+
+class MyState : BaseState() {
+}
+
+@Service(Service.Level.PROJECT)
+@State(name = "cmake-source-list-sorter-config")
+class ConfigManagerService(private val project: Project) : SimplePersistentStateComponent<MyState>(MyState()) {
+}
+
+
